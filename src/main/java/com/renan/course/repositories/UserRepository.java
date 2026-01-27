d460d69d126e4e9eb128633bf562310d3f4e549f
Renan Costa <renandev.rj@gmail.com>
2026-01-27 14:20:57 -0300

    Service layer, component registration

--- src/main/java/com/renan/course/repositories/UserRepository.java
+++ src/main/java/com/renan/course/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.renan.course.repositories;
 
 import com.renan.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
