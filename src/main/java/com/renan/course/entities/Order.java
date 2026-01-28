681aa3313a0d46218d6e94bd964b86ad051cd655
Renan Costa <renandev.rj@gmail.com>
2026-01-28 10:16:22 -0300

    OrdersStatus enum

--- src/main/java/com/renan/course/entities/Order.java
+++ src/main/java/com/renan/course/entities/Order.java
@@ -2,6 +2,7 @@ package com.renan.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.renan.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 import org.springframework.boot.jackson.autoconfigure.JacksonProperties;
 
@@ -23,6 +24,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -30,9 +33,10 @@ public class Order implements Serializable {
 
 
     public Order(){}
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -49,6 +53,14 @@ public class Order implements Serializable {
     public void setMoment(Instant moment) {
         this.moment = moment;
     }
+    public OrderStatus getOrderStatus() {return OrderStatus.valueOff(orderStatus);}
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
     public User getClient() {
         return client;
     }
