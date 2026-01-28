2bb14d6c5444ea206a8319d2e566c0d56f9cb8d0
Renan Costa <renandev.rj@gmail.com>
2026-01-28 11:46:37 -0300

    Product implementation

--- src/main/java/com/renan/course/entities/Category.java
+++ src/main/java/com/renan/course/entities/Category.java
@@ -3,7 +3,9 @@ package com.renan.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -14,6 +16,10 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+
+
+    @Transient
+    private Set<Product> products = new HashSet<>();
 
 
     public Category(){}
@@ -27,7 +33,7 @@ public class Category implements Serializable {
     public void setId(Integer id) {this.id = id;}
     public String getName() {return name;}
     public void setName(String name) {this.name = name;}
-
+    public Set<Product> getProducts() {return products;}
 
     @Override
     public boolean equals(Object o) {
---
+++ src/main/java/com/renan/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.renan.course.repositories;
+
+import com.renan.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
